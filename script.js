05dcc8976afad3e8709244e2a5afc8aa0a83290a
kentamortizado <69563000+kentamortizado@users.noreply.github.com>
2025-02-01 10:06:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ document.getElementById("rsvp-form").addEventListener("submit", function(event) 
     const attendance = document.getElementById("attendance").value;
     const responseMessage = document.getElementById("response-message");
 
-    fetch("https://script.google.com/macros/s/YOUR_GOOGLE_SCRIPT_URL/exec", {
+    fetch("https://script.google.com/macros/s/AKfycbz0LSQ-3hg_Th08w1gbfTLvoh_wiUfd_TSinZN_DPzLAmRR4Isq4T3l7OCOoXjBsmXDuQ/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, attendance })
