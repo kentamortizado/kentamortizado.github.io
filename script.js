39c106e3dfee44d625021bf0ae2e7d682922bce3
Kent A <kenta.@Kents-MacBook-Air.local>
2025-02-02 14:19:30 +0800

    Initial commit 50

--- script.js
+++ script.js
@@ -54,3 +54,33 @@ const countdown = setInterval(function() {
         document.getElementById("timer").innerHTML = "The big day has arrived!";
     }
 }, 1000);
+
+
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slider = document.querySelector('.slider');
+    const totalSlides = document.querySelectorAll('.slider img').length;
+    
+    if (index >= totalSlides) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = totalSlides - 1;
+    } else {
+        currentIndex = index;
+    }
+    
+    const offset = -currentIndex * 100 + '%';
+    slider.style.transform = `translateX(${offset})`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+// Auto-slide every 3 seconds
+setInterval(nextSlide, 3000);
